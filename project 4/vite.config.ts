d33ce53dbbd2e99d0fb1a63258c3926dd9548a62
2woot111 <sean@series.so>
2025-01-19 18:31:53 -0500

    Update vite.config.ts

--- project 4/vite.config.ts
+++ project 4/vite.config.ts
@@ -4,15 +4,26 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
   build: {
     outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
+      input: {
+        main: './index.html'
+      },
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom', 'react-router-dom'],
           'supabase-vendor': ['@supabase/supabase-js']
-        }
+        },
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       }
     }
   }
